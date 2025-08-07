491028dbfde4549535eec480d2dbb4b2110f9ee5
Devlopersoumya <soumya23.microsoftgmail.com>
2025-08-07 16:54:40 +0530

    dat data removed

--- src/components/AllFood.jsx
+++ src/components/AllFood.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios, { all } from 'axios'
-import allCategories from '../data/data';
+import axios from 'axios'
 import { useNavigate } from 'react-router';
 
 const AllFood = () => {
